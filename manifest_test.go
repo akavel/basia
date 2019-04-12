440b2f8d4d85e8763bfe9af37efdecf1739b0073
Mateusz Czapliński <czapkofan@gmail.com>
2019-04-12 23:14:58 +0200

    manifest: add (failing) WriteTo test

--- manifest_test.go
+++ manifest_test.go
@@ -1,21 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"strings"
 	"testing"
 
+	differ "github.com/kylelemons/godebug/diff"
 	"github.com/kylelemons/godebug/pretty"
 )
 
-func TestParseManifest(test *testing.T) {
+func TestParseAndWriteManifest(test *testing.T) {
 	cases := []struct {
-		comment string
-		input   string
-		want    Manifest
+		comment    string
+		serialized string
+		parsed     Manifest
 	}{
 		{
 			comment: "multiline names",
-			input: `Manifest-Version: 1.0
+			serialized: strings.ReplaceAll(`Manifest-Version: 1.0
 Built-By: Generated-by-ADT
 Created-By: Android Gradle 3.3.2
 
@@ -26,8 +28,8 @@ SHA1-Digest: x6OHiSoyMWiuIOgpmUuAh/tRnYM=
 Name: res/drawable/abc_list_selector_background_transition_holo_light.
  xml
 SHA1-Digest: 0fvC1p6NZOpNNtjO4w0DBYRz8d0=
-`,
-			want: Manifest{
+`, "\n", "\r\n"),
+			parsed: Manifest{
 				"": Attributes{
 					`Manifest-Version: 1.0`,
 					`Built-By: Generated-by-ADT`,
@@ -44,13 +46,19 @@ SHA1-Digest: 0fvC1p6NZOpNNtjO4w0DBYRz8d0=
 	}
 
 	for _, c := range cases {
-		have, err := ParseManifest(strings.NewReader(c.input))
+		parsed, err := ParseManifest(strings.NewReader(c.serialized))
 		if err != nil {
 			test.Errorf("%q: %s", c.comment, err)
-			continue
+		} else if diff := pretty.Compare(parsed, c.parsed); diff != "" {
+			test.Errorf("%q: ParseManifest diff (-have +want):\n%s", c.comment, diff)
 		}
-		if diff := pretty.Compare(have, c.want); diff != "" {
-			test.Errorf("%q: diff (-have +want):\n%s", c.comment, diff)
+
+		serialized := bytes.NewBuffer(nil)
+		_, err = c.parsed.WriteTo(serialized)
+		if err != nil {
+			test.Errorf("%q: %s", c.comment, err)
+		} else if diff := differ.Diff(serialized.String(), c.serialized); diff != "" {
+			test.Errorf("%q: WriteTo diff (-have +want):\n%s", c.comment, diff)
 		}
 	}
 }
